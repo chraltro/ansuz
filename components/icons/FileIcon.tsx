c8229a773f47ffb38c8bcfa27b3b681cc88bf491
Christian Demant <cnd@banknorwegian.no>
2025-10-15 19:13:48 +0200

    Improvements

--- components/icons/FileIcon.tsx
+++ components/icons/FileIcon.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 
-const FileIcon: React.FC<{ className?: string }> = ({ className = 'w-5 h-5' }) => (
+const FileIcon: React.FC<{ className?: string }> = React.memo(({ className = 'w-5 h-5' }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     className={className}
@@ -16,6 +16,8 @@ const FileIcon: React.FC<{ className?: string }> = ({ className = 'w-5 h-5' }) =
       d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
     />
   </svg>
-);
+));
+
+FileIcon.displayName = 'FileIcon';
 
 export default FileIcon;

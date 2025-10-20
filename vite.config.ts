2b939d3b8370b049238ae2a484954790be984464
Christian Demant <christian@altro.no>
2025-10-20 12:24:51 +0200

    Google auth

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     base: process.env.NODE_ENV === 'production' ? '/ansuz/' : '/',
+    publicDir: 'public',
     plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
